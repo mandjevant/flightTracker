c710a7e507169fe974d254802fdc94f30decbd25
Tholhuijsen, P.T.C.M <p.t.c.m.tholhuijsen@student.tue.nl>
2020-09-17 18:17:12 +0200

    test statevector

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, redirect, render_template, session, url_for
+from opensky_api import OpenSkyApi, StateVector
 import configparser
+import json
 import sys
 import os
 
@@ -31,6 +33,31 @@ if not Config.initiate_config():
 
 app = Flask(__name__)
 app.secret_key = Config.conf.get('CONSTANTS', 'SECRET_KEY')
+openskyAPI = OpenSkyApi(username=Config.conf.get('OPENSKY', 'USERNAME'),
+                        password=Config.conf.get('OPENSKY', 'PASSWORD'))
+
+
+def jsonify_vector(vector: StateVector):
+    return jsonify(
+        {'baro_altitude': vector.baro_altitude,
+         'callsign': vector.callsign,
+         'geo_altitude': vector.geo_altitude,
+         'heading': vector.heading,
+         'icao24': vector.icao24,
+         'last_contact': vector.last_contact,
+         'latitude': vector.latitude,
+         'longitude': vector.longitude,
+         'on_ground': vector.on_ground,
+         'origin_country': vector.origin_country,
+         'position_source': vector.position_source,
+         'sensors': vector.sensors,
+         'spi': vector.spi,
+         'squawk': vector.squawk,
+         'time_position': vector.time_position,
+         'velocity': vector.velocity,
+         'vertical_rate': vector.vertical_rate
+         }
+    )
 
 
 @app.route('/')
@@ -41,6 +68,13 @@ def main():
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
+
+
+@app.route('/openskytest')
+def opensky_test():
+    states = openskyAPI.get_states()
+
+    return jsonify_vector(states.states[0])
 
 
 if __name__ == '__main__':

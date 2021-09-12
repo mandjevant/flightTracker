706b83fdfe95213c123984c2f23e35ddde2062d5
mandjevant <38689620+mandjevant@users.noreply.github.com>
2021-09-12 14:39:47 +0200

    Documentation wrapper

--- aviationstackWrapper.py
+++ aviationstackWrapper.py
@@ -3,15 +3,35 @@ import requests
 
 
 class aviationstackApi:
-    def __init__(self, base_url="http://api.aviationstack.com/v1/flights"):
+    """
+    Wrapper class aviationstack API
+    """
+
+    def __init__(self, base_url: str = "http://api.aviationstack.com/v1/flights"):
+        """
+        Initiate class variables
+        :param base_url: base url for aviationstack API | str
+        """
         self.base_url = base_url
         self.URL = self.base_url
 
     def add_app_credentials(self):
+        """
+        Supplement base url with application credentials
+        """
         self.URL += f"?access_key={aviationStackAccessKey}"
 
     def add_flight_iata(self, courier, number):
+        """
+        Supplement url with flight iata for querying
+        :param courier: courier
+        :param number: flight number
+        """
         self.URL += f"&flight_iata={courier}{number}"
 
     def get(self):
+        """
+        Perform API get request
+        :return: request
+        """
         return requests.get(self.URL)
